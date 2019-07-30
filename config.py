35f28a5cad290a54f65f1cb1a388d2d357997425
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2019-07-30 12:26:26 -0400

    progress

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
+import numpy as np
+import copy
 from glue.config import data_factory
 from glue.core import Data, Component
-from skimage.io import imread
 from casatools import table
 
 def is_measurement_set(filename, **kwargs):
@@ -11,13 +12,24 @@ def read_measurement_set(file_name):
     tb = table()
     tb.open(file_name)
     columns = tb.colnames()
+    columns.remove('DATA')
     datadict = {}
+    fulldata = MeasurementSetComponent(file_name, 'DATA')
+    realdata = copy.copy(fulldata)
+    realdata._data = fulldata._data.real
+    imagdata = copy.copy(fulldata)
+    imagdata._data = fulldata._data.imag
+    datadict['REAL_DATA'] = realdata
+    datadict['IMAG_DATA'] = imagdata
+    datashape = datadict['REAL_DATA'].data.shape
     for cn in columns:
         try:
-            datadict[cn] = MeasurementSetComponent(file_name, cn)
+            datadict[cn] = MeasurementSetComponent(file_name, cn, shape=datashape)
         except RuntimeError:
             # data do not exist or can't be read for some reason
             pass
+        except ValueError:
+            print(f"FAILED to load {cn}")
     result = CASAData(**datadict)
     tb.close()
     return result
@@ -26,7 +38,7 @@ class CASAData(Data):
     pass
 
 class MeasurementSetComponent(Component):
-    def __init__(self, filename, colname):
+    def __init__(self, filename, colname, shape=None):
         self.filename = filename
         self.colname = colname
 
@@ -35,6 +47,8 @@ class MeasurementSetComponent(Component):
             tb = table()
             tb.open(self.filename)
             self._data = tb.getcol(self.colname)
+            if shape is not None:
+                self._data = np.broadcast_to(self._data, shape)
             tb.close()
 
     @property
